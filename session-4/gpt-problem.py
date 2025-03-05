353cfb08bc69cf0f365cadf62bacca64e5dd984c
AminaManseur29 <amina.manseur@ensae.fr>
2025-03-05 11:07:26 +0100

    Session 4

--- session-4/gpt-problem.py
+++ session-4/gpt-problem.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
@@ -69,23 +70,96 @@ def estimate_loss():
     return out
 
 # Copy your Head, MultiHeadAttention, FeedForward and Block classes here
+class Head(nn.Module):
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)   
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.dropout = nn.Dropout(0.1)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size))) # store a persistent buffer for the forward pass
+
+    def forward (self, x):
+        B, T, C = x.shape
+        keys = self.key(x)
+        queries = self.query(x)
+        values = self.value(x)
+        wei = torch.matmul(queries, keys.transpose(-2, -1))
+        wei = wei / math.sqrt(C)
+        wei = wei.masked_fill(self.tril==0, float('-inf'))
+        wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
+        out = torch.matmul(wei, values)
+        return out
     
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.linear = nn.Linear(num_heads * head_size, n_embd)
+
+    def forward (self, x):
+        heads = [h(x) for h in self.heads]
+        out = torch.cat(heads, dim=-1)
+        out = self.linear(out)
+        return out
+    
+class FeedForward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+    def __init__(self, n_embd):
+        super().__init__()
+        self.linear1 = nn.Linear(n_embd, 4*n_embd)
+        self.activation = nn.ReLU()
+        self.linear2 = nn.Linear(4*n_embd, n_embd)
+
+    def forward(self, x):
+        out = self.linear1(x)
+        out = self.activation(out)
+        out = self.linear2(out)
+        return out
+
+class Block(nn.Module):
+
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+        self.attention = MultiHeadAttention(n_head, head_size)
+        self.ff = FeedForward(n_embd)
+
+    def forward(self, x):
+        out = self.ln1(x)
+        out = self.attention(out)
+        int = out + x
+        out = self.ln2(int)
+        out = self.ff(out)
+        out = out + int
+        return out
+
 class GPT(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.token_embedding_table = ???
-        self.pos_embedding_table = ???
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.pos_embedding_table = nn.Embedding(block_size, n_embd)
         # define blocks, a layer norm and a linear layer
-        ???
+        self.blocks = nn.ModuleList([Block(n_embd, n_heads) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd)
+        self.head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
         B, T = idx.shape
-        token_emb = ??? # (B,T,C)
+        token_emb = self.token_embedding_table(idx) # (B, T, C)
         pos_emb = self.pos_embedding_table(torch.arange(T, device=device)) # (T, C)
-        x = ??? # sum the token embeddings and position embeddings
-        ??? # apply blocks, layer norm and linear layer (leading to the logits variable)
-
+        x = token_emb + pos_emb # (B, T, C) # sum the token embeddings and position embeddings
+        # apply blocks, layer norm and linear layer (leading to the logits variable)
+        for block in self.blocks:
+            x = block(x)
+        x = self.ln_f(x)
+        logits = self.head(x) # (B, T, V)
+    
         # do not modify the rest of the method (it computes the loss during the forward pass)
         if targets is None:
             loss = None
